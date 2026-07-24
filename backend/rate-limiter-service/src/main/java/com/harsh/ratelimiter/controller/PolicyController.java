a934e6dd1a7ab499a6ce6d68aa6d1e9caa8d08f8
Harsh <harsh0511r@gmail.com>
2026-07-24 22:49:12 +0530

    Update Controller

--- backend/rate-limiter-service/src/main/java/com/harsh/ratelimiter/controller/PolicyController.java
+++ backend/rate-limiter-service/src/main/java/com/harsh/ratelimiter/controller/PolicyController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,4 +37,12 @@ public class PolicyController {
                 .body(response);
     }
 
+    @GetMapping("/{clientId}")
+    public ResponseEntity<PolicyResponse> getPolicy(
+            @PathVariable String clientId) {
+
+        PolicyResponse response = policyService.getPolicy(clientId);
+
+        return ResponseEntity.ok(response);
+    }
 }
